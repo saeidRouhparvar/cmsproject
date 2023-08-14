01580d74846b2e3afbdc2f6dff18074526e00325
saeid <saeid.rohparvar@gmail.com>
2023-08-14 10:01:45 +0330

    navItem refactor

--- src/layouts/sidebar/NavItem.tsx
+++ src/layouts/sidebar/NavItem.tsx
@@ -1,21 +1,26 @@
-import { Box, BoxProps, Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Box, BoxProps, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Routes } from "../../routes/routes";
 
 interface Interface extends BoxProps {
-    reset?:any
-    path?:any
-    children?:any
-    ShowInNav:?any
+  reset?: any;
+  path?: any;
+  children?: any;
+  ShowInNav?: any;
 }
 
-function NavItem({path, children, ShowInNav}:Interface) {
+const NavItem = ({ children }: Interface) => {
   return (
-    <Flex h={"100vh"} w={"192px"} bg={"#FFFFFF"} borderLeftRadius={"25px"}>
-<Box>
-    {children}
-</Box>
+    <Flex
+      h={"100vh"}
+      w={"192px"}
+      bg={"#FFFFFF"}
+      borderLeftRadius={"25px"}
+      direction={"column"}
+    >
+      <Box>{children}</Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
