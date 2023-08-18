dcaa2c9555ec7c2b2864c1a6715b3907937c704a
saeid <saeid.rohparvar@gmail.com>
2023-08-19 00:17:38 +0330

    refactor routes name

--- src/layouts/sidebar/SideBarLayout.tsx
+++ src/layouts/sidebar/SideBarLayout.tsx
@@ -1,19 +1,18 @@
 import { Flex, Box } from "@chakra-ui/react";
 import NavItem from "./NavItem";
-import { Routes } from "../../routes/Routes";
+import { IndexRoutes } from "../../routes/routes";
 import { useState } from "react";
-import { Scrollbars } from "rc-scrollbars";
+// import { Scrollbars } from "rc-scrollbars";
 import { OnOfIcon } from "../../assets/Svg";
 
 const SideBarLayout = () => {
-  const sideList: any = Routes.filter((item: any) => item.path === "/*")[0]
+  const sideList: any = IndexRoutes.filter((item: any) => item.path === "/*")[0]
     .child;
   const [active, setActive] = useState(false);
 
   return (
     // <Scrollbars style={{width: "100%", height: "100vh"}} >
-    <Box h={"2000px"} bg={"#F8FAFF"}
-    >
+    <Box h={"2000px"} bg={"#F8FAFF"}>
       <Flex
         direction={"column"}
         gap={{ base: "25px", md: "52px" }}
