d371d45b3c5de54432f79a47b3ed6cc95263fd10
saeid <saeid.rohparvar@gmail.com>
2023-08-18 12:31:28 +0330

    svg file refactor

--- src/components/header/Revenue.tsx
+++ src/components/header/Revenue.tsx
@@ -1,41 +1,23 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import { ArrowDownIcon, ArrowUpIcon } from "../../assets/Svg";
 
 const Revenue = () => {
   return (
     <Flex align={"center"}>
-      <Box fontSize={"28px"} fontWeight={600}>$</Box>
-      <Box fontSize={"28px"} fontWeight={600}>45,365.00</Box>
+      <Box fontSize={"28px"} fontWeight={600}>
+        $
+      </Box>
+      <Box fontSize={"28px"} fontWeight={600}>
+        45,365.00
+      </Box>
       <Flex fontSize={"12px"} fontWeight={500}>
         <Flex ms={"7px"} align={"center"}>
-          <Box>
-            <svg
-              width="13"
-              height="10"
-              viewBox="0 0 13 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6.5 10L0.870835 0.249999L12.1292 0.25L6.5 10Z"
-                fill="#FF4588"
-              />
-            </svg>
-          </Box>
+          <Box>{ArrowDownIcon}</Box>
           <Box color={"#FF4588"}>$1,294</Box>
         </Flex>
         <Flex ms={"7px"} align={"center"}>
-          <Box>
-            <svg
-              width="13"
-              height="10"
-              viewBox="0 0 13 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M6.5 0L12.1292 9.75H0.870835L6.5 0Z" fill="#19C332" />
-            </svg>
-          </Box>
+          <Box>{ArrowUpIcon}</Box>
           <Box color={"#19C332"}>$1,294</Box>
         </Flex>
       </Flex>
