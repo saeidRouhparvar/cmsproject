f899eaf44c8c43ef5a3a0f3ae046e2533c51fb45
saeid <saeid.rohparvar@gmail.com>
2023-08-19 00:10:45 +0330

    emptyError refactor

--- src/components/error/emptyError.tsx
+++ src/components/error/emptyError.tsx
@@ -2,15 +2,26 @@ import { Box, BoxProps } from "@chakra-ui/react";
 
 interface Interface extends BoxProps {
   reset?: any;
-  message?: string;
+  message?: any;
 }
 
-const emptyError = ({ message, ...reset }: Interface) => {
+const EmptyError = ({ message, ...reset }: Interface) => {
   return (
-    <Box bg={"red"} {...reset}>
+    <Box
+      mt={"50px"}
+      w={"full"}
+      p={"30px"}
+      fontSize={"28px"}
+      fontWeight={600}
+      bg={"red.300"}
+      textAlign={"center"}
+      borderRadius={"15px"}
+      border={"2px solid red"}
+      {...reset}
+    >
       {message}
     </Box>
   );
 };
 
-export default emptyError;
+export default EmptyError;
